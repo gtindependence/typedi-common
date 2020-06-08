18af4b8f3cabb059a9944d5e07e32cceab9519f9
John Pinkster <john.c.pinkster@gmail.com>
2020-06-08 11:33:53 -0400

    feat(info): log out breadcrumb data

--- src/modules/services/error-handler.service.ts
+++ src/modules/services/error-handler.service.ts
@@ -44,7 +44,7 @@ export class ErrorHandler {
             Raven.captureBreadcrumb(breadcrumb);
         }
         else {
-            this.logger.info(breadcrumb.message);
+            this.logger.info(breadcrumb.message, breadcrumb.data);
         }
     }
 
