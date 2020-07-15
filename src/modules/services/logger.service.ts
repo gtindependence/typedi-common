4aa1f869b784dba3d0e384c8c45040a1d61efd74
John Pinkster <john.c.pinkster@gmail.com>
2020-07-15 16:26:28 -0400

    feat(logger): upgrading to the latest log4js library

--- src/modules/services/logger.service.ts
+++ src/modules/services/logger.service.ts
@@ -1,4 +1,3 @@
-import * as config from 'config';
 import * as log from 'log4js';
 import { Service } from 'typedi';
 
@@ -7,18 +6,7 @@ export class LoggerService {
     logger: log.Logger;
 
     constructor() {
-        let level: string;
-        try {
-            level = config.get('logger.level');
-        }
-        catch {
-            // tslint:disable-next-line:no-console
-            console.warn('You need to create a config for logger.level. See the README in @teamhive/typedi-common');
-            level = 'debug';
-        }
-
         this.logger = log.getLogger();
-        this.logger.level = level;
     }
 
     info(message: any, ...args: any[]) {
@@ -28,5 +16,4 @@ export class LoggerService {
     error(message: any, ...args: any[]) {
         this.logger.error(message, ...args);
     }
-
 }
